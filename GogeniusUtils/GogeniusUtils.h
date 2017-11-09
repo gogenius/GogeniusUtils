e0c5102d629f9820da5b77f5cbae01495c569e58
Gogenius <dev@gogen.com.cn>
2017-11-09 10:06:55 +0800

    init

---
+++ GogeniusUtils/GogeniusUtils.h
@@ -0,0 +1,19 @@
+//
+//  GogeniusUtils.h
+//  GogeniusUtils
+//
+//  Created by Neo on 2017/11/9.
+//  Copyright © 2017年 GOGenius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GogeniusUtils.
+FOUNDATION_EXPORT double GogeniusUtilsVersionNumber;
+
+//! Project version string for GogeniusUtils.
+FOUNDATION_EXPORT const unsigned char GogeniusUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GogeniusUtils/PublicHeader.h>
+
+
